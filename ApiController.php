b350ce230118b5398955ab69217ab143bfecbad4
Shawn Rice <zooley@purdue.edu>
2015-06-06 14:22:10 -0400

    [VENDOR][LIB_*] Deep breath, folks. Moving to core.

---
+++ ApiController.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2014 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2014 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+namespace Hubzero\Component;
+
+use Hubzero\Http\Request;
+use Hubzero\Http\Response;
+
+/**
+ * Base API controller for components to extend.
+ */
+class ApiController implements ControllerInterface
+{
+	/**
+	 * Description for '_response'
+	 *
+	 * @var unknown
+	 */
+	protected $_response = null;
+
+	/**
+	 * Description for '_request'
+	 *
+	 * @var unknown
+	 */
+	protected $_request = null;
+
+	/**
+	 * Description for '_provider'
+	 *
+	 * @var unknown
+	 */
+	protected $_provider = null;
+
+	/**
+	 * Description for '_segments'
+	 *
+	 * @var array
+	 */
+	protected $segments = array();
+
+	/**
+	 * Determines task being called and attempts to execute it
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		$response = new \stdClass();
+		$response->error->code    = 500;
+		$response->error->message = \Lang::txt('Component must implement the execute() method.');
+
+		$this->getResponse()
+		     ->setErrorMessage($response->error->code, $response->error->message);
+
+		$this->setMessageType(\Request::getWord('format', 'json'));
+		$this->setMessage($response);
+	}
+
+	/**
+	 * Short description for 'setRequest'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @param      unknown $request Parameter description (if any) ...
+	 * @return     void
+	 */
+	function setRequest(Request $request)
+	{
+		$this->_request = $request;
+	}
+
+	/**
+	 * Short description for 'getRequest'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @return     unknown Return description (if any) ...
+	 */
+	function getRequest()
+	{
+		return $this->_request;
+	}
+
+	/**
+	 * Short description for 'setResponse'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @param      unknown $response Parameter description (if any) ...
+	 * @return     void
+	 */
+	function setResponse(Response $response)
+	{
+		$this->_response = $response;
+	}
+
+	/**
+	 * Short description for 'getResponse'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @return     unknown Return description (if any) ...
+	 */
+	function getResponse()
+	{
+		return $this->_response;
+	}
+
+	/**
+	 * Short description for 'setProvider'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @param      unknown $provider Parameter description (if any) ...
+	 * @return     void
+	 */
+	function setProvider($provider)
+	{
+		$this->_provider = $provider;
+	}
+
+	/**
+	 * Short description for 'getProvider'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @return     unknown Return description (if any) ...
+	 */
+	function getProvider()
+	{
+		return $this->_provider;
+	}
+
+	/**
+	 * Short description for 'setRouteSegments'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @param      unknown $segments Parameter description (if any) ...
+	 * @return     void
+	 */
+	function setRouteSegments($segments)
+	{
+		$this->segments = $segments;
+	}
+
+	/**
+	 * Short description for 'getRouteSegments'
+	 *
+	 * Long description (if any) ...
+	 *
+	 * @return     array Return description (if any) ...
+	 */
+	function getRouteSegments()
+	{
+		return $this->segments;
+	}
+
+	function setMessage($message = null, $status = null, $reason = null)
+	{
+		//$this->_response->setMessage($message, $status, $reason);
+		//$this->_response->setContent($this->finalizeContent($message));
+		$this->_response->setContent($message);
+		$this->_response->setStatusCode($status ? $status : 200);
+	}
+
+	function setMessageType($format)
+	{
+		//$this->_response->setResponseProvides($format);
+		static $types = array(
+			'xml' => 'application/xml',
+			'html' => 'text/html',
+			'xhtml' => 'application/xhtml+xml',
+			'json' => 'application/json',
+			'text' => 'text/plain',
+			'txt' => 'text/plain',
+			'plain' => 'text/plain',
+			'php_serialized' => 'application/vnd.php.serialized',
+			'php' => 'application/php',
+		);
+
+		$this->_response->headers->set('Content-Type', $types[$format]);
+	}
+}
+
---
+++ ComponentServiceProvider.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+namespace Hubzero\Component;
+
+use Hubzero\Base\Middleware;
+use Hubzero\Http\Request;
+
+/**
+ * Component loader service provider
+ */
+class ComponentServiceProvider extends Middleware //ServiceProvider
+{
+	/**
+	 * Register the service provider.
+	 *
+	 * @return  void
+	 */
+	public function register()
+	{
+		$this->app['component'] = function($app)
+		{
+			return new Loader($app);
+		};
+	}
+
+	/**
+	 * Handle request in HTTP stack
+	 * 
+	 * @param   object  $request  HTTP Request
+	 * @return  mixed
+	 */
+	public function handle(Request $request)
+	{
+		$response = $this->next($request);
+
+		if (!$this->app->runningInConsole())
+		{
+			$component = $request->getCmd('option');
+			if (!$component)
+			{
+				$this->app->abort(404);
+			}
+
+			$contents = $this->app['component']->render($component);
+
+			$response->setContent($contents);
+
+			$this->app['dispatcher']->trigger('system.onAfterDispatch');
+
+			if ($this->app->has('profiler'))
+			{
+				$this->app['profiler'] ? $this->app['profiler']->mark('afterDispatch') : null;
+			}
+		}
+
+		return $response;
+	}
+}
